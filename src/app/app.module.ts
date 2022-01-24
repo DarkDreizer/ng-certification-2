7192935a8204bca3b92e3e2ff8455b74e270880b
Carlos Lopez <clopez@applaudostudios.com>
2022-01-23 20:30:22 -0600

    move templates into components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { MainComponent } from './components/main/main.component';
+import { ForecastComponent } from './components/forecast/forecast.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, MainComponent, ForecastComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
