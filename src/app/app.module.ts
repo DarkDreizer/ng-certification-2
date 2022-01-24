be40e0533c546245b6365e43e3e17f8ea62887ae
Carlos Lopez <clopez@applaudostudios.com>
2022-01-23 21:37:43 -0600

    add not found component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,14 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { MainComponent } from './components/main/main.component';
 import { ForecastComponent } from './components/forecast/forecast.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { AppRoutingModule } from './app-routing.module';
+import { APP_BASE_HREF } from '@angular/common';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, MainComponent, ForecastComponent ],
+  imports:      [ BrowserModule, FormsModule, AppRoutingModule ],
+  declarations: [ AppComponent, HelloComponent, MainComponent, ForecastComponent, NotFoundComponent ],
+  providers: [{provide: APP_BASE_HREF, useValue: '/'}],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
